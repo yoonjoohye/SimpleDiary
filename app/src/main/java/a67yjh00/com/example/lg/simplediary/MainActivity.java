413945e1206ef95893fc13f773456eba2bb00a99
yoonjoohye <67yjh00@gmail.com>
2017-05-30 20:24:20 +0900

    #2 Assign View reference

--- app/src/main/java/a67yjh00/com/example/lg/simplediary/MainActivity.java
+++ app/src/main/java/a67yjh00/com/example/lg/simplediary/MainActivity.java
@@ -2,12 +2,20 @@ package a67yjh00.com.example.lg.simplediary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    DatePicker date;
+    EditText edit;
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        date = (DatePicker)findViewById(R.id.date);
+        edit = (EditText)findViewById(R.id.edit);
+        but = (Button)findViewById(R.id.but);
     }
 }
